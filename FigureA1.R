561ac31f875ca8e8a19d9a709163bf6913f87d9b
Andres Quinones <andresqp@gmail.com>
2019-06-30 22:55:40 -0500

    Adjust Figure A1 to new notation

--- FigureA1.R
+++ FigureA1.R
@@ -4,10 +4,10 @@
 
 
 # Directory with the IBS -------------------------------------------------------
-Simdir<-"E:\\Dropbox\\Haplodiploidy\\Gil\\Andres\\IBD"
+library('here')
+source('SelecDiff_Evol.R')
 
-
-png("E:\\Dropbox\\Haplodiploidy\\Gil\\Andres\\Evolution\\Figure_A1_dynamics.png",
+png(here("Figure_A1_dynamics.png"),
     width=210*3,height=297*3)
 
 
@@ -15,10 +15,10 @@ parsize<-1.3
 labsize<-2.5
 # Colour ribbons
 colorPol<-c("#a1d99b","#fed976","#fed976","paleturquoise3","#bcbddc")
+colors1<-c("#31a354","#d95f0e","#d95f0e","#3182bd","Purple")
 
 
-
-list1<-list.files()
+list1<-list.files(here('IBD'))
 list1<-list1[grep('09',x=list1)]
 listEv<-list1[grep('evol',x = list1)]
 list.dist<-list1[grep('dist',x = list1)]
@@ -33,10 +33,10 @@ plot.new()
 for(seed in 1:10){
   if(county==1){county<-6;countx<-countx+1}
   county<-county-1
-  Xs <- read.table(listEv[seed])
-  names(Xs) <- c("Gen","z1","z2","z3","h","om","sdz1","sdz2","sdz3","sdh","sdom",
-                 "Q1z1","Q1z2","Q1z3","Q1h","Q1om",
-                 "Q3z1","Q3z2","Q3z3","Q3h","Q3om")
+  Xs <- read.table(here('IBD',listEv[seed]))
+  names(Xs) <- c("Gen","z1","z3","z5","h","om","sdz1","sdz3","sdz5","sdh","sdom",
+                 "Q1z1","Q1z3","Q1z5","Q1h","Q1om",
+                 "Q3z1","Q3z3","Q3z5","Q3h","Q3om")
   par(las=1,plt=posPlot(numploty=5,idploty=county,numplotx=2,idplotx=countx),
       xpd=F,yaxt=yaxs[countx],xaxt=xaxs[county],new=T)
   plot(x=c(0,20000),y=c(0.5,0.5),type="l",lwd=2,col="grey",ylim=c(0,1),
@@ -46,10 +46,10 @@ for(seed in 1:10){
           y=c(Xs$Q3z1,Xs$Q1z1[length(Xs$sdz1):1]),
           col=colorPol[1],border=NA)
   polygon(x=c(Xs$Gen,Xs$Gen[length(Xs$Gen):1]),
-          y=c(Xs$Q3z2,Xs$Q1z2[length(Xs$sdz2):1]),
+          y=c(Xs$Q3z3,Xs$Q1z3[length(Xs$sdz3):1]),
           col=colorPol[2],border=NA)
   polygon(x=c(Xs$Gen,Xs$Gen[length(Xs$Gen):1]),
-          y=c(Xs$Q3z3,Xs$Q1z3[length(Xs$sdz3):1]),
+          y=c(Xs$Q3z5,Xs$Q1z5[length(Xs$sdz5):1]),
           col=colorPol[3],border=NA)
   polygon(x=c(Xs$Gen,Xs$Gen[length(Xs$Gen):1]),
           y=c(Xs$Q3h,Xs$Q1h[length(Xs$sdh):1]),
@@ -58,8 +58,8 @@ for(seed in 1:10){
           y=c(Xs$Q3om,Xs$om[length(Xs$om):1]-Xs$sdom[length(Xs$sdom):1]),
           col=colorPol[5],border=NA)
   lines(Xs$Gen,Xs$z1,lwd=3,col=colors1[1])
-  lines(Xs$Gen,Xs$z2,lwd=3,col=colors1[2])
-  lines(Xs$Gen,Xs$z3,lwd=3,col=colors1[3],lty=2)
+  lines(Xs$Gen,Xs$z3,lwd=3,col=colors1[2])
+  lines(Xs$Gen,Xs$z5,lwd=3,col=colors1[3],lty=2)
   lines(Xs$Gen,Xs$h,lwd=3,col=colors1[4])
   lines(Xs$Gen,Xs$om,lwd=3,col=colors1[5])
   box()
